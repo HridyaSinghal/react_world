dab42f34ac19e967ea1378e3198a3f8f3a02f6c9
Hridya Singhal <hridyasinghal08@gmail.com>
2025-09-10 13:44:26 +0530

    Initail

---
+++ zero2counter/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+const[val,setCounter] = useState(10);
+
+
+
+  return (
+    <>
+      <h1>counter value {val} </h1>
+      <button onClick = {()=>{
+        if(val<20){
+        setCounter(val+1)}
+        }}>Increase</button>
+      <button onClick={() => setCounter(val-1)}>Decrease</button>
+    </>
+  )
+}
+
+export default App
